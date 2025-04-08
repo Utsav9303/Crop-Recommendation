d8a99a9bc7556a3955873848728e802bb777582e
Utsav9303 <utsavmodi000@gmail.com>
2025-04-08 21:56:39 +0530

    first commit

---
+++ server/server.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+model = pickle.load(open('./artifact/crop_prediction_model.pkl', 'rb'))
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[]
+    json = request.get_json()
+    int_features.append(float(json['N']))
+    int_features.append(float(json['P']))
+    int_features.append(float(json['K']))
+    int_features.append(float(json['temperature']))
+    int_features.append(float(json['humidity']))
+    int_features.append(float(json['pH']))
+    int_features.append(float(json['rainfall']))
+
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = prediction[0]
+    obj = {
+        "res":"Success",
+        "output":str(output)
+    }
+    return jsonify(obj)
+
+if __name__ == "__main__":
+    app.run(debug=True)
